a7276fa546db52c20852a15c7aea2fb13791330b
satyamsinha20 <skumarsinha1979@gmail.com>
2025-12-01 15:11:48 +0530

    Updated frontend UI / bug fixes / new features

--- frontend/src/api/chat.js
+++ frontend/src/api/chat.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://spichat.onrender.com/api",
 });
 
 export const getMyConversations = async (token) => {
